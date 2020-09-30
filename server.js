0950e210d9cfcf844664fe151e4c37f83cf3558f
msyphus <mark.syphus@gmail.com>
2020-09-30 11:15:15 -0600

    uncommented path.join command

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
     app.use(express.static("/client/build"));
     // Express will serve up the front-end index.html file if it doesn't recognize the route
-    // app.get('*', function (req, res) {
-    //     const index = path.join(__dirname, '/client/build', 'index.html');
-    //     res.sendFile(index);
-    // });
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, '/client/build', 'index.html');
+        res.sendFile(index);
+    });
 }
 
 app.use(routes);
