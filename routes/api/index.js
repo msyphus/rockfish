f887f7222c3f0ee0c626ab50465ad822d6b45daa
msyphus <mark.syphus@gmail.com>
2020-10-02 15:14:16 -0600

    added default rendering to api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,4 +3,8 @@ const creelRoutes = require("./creels");
 
 router.use("/creels", creelRoutes);
 
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
 module.exports = router;
