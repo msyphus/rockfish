ff80ec902e70817bf776d1775494a062e448836f
msyphus <mark.syphus@gmail.com>
2020-10-07 11:34:07 -0600

    started converting to Sequelize: changed connection, npm packages, seed data

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const routes = require("./routes");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
-const routes = require("./routes");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const seeds = require("./scripts/seedDB")
+require ("dotenv").config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,24 +16,15 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     // Express will serve up production assets
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "/client/build")));
     // Express will serve up the front-end index.html file if it doesn't recognize the route
     app.get('*', function (req, res) {
-        const index = path.join(__dirname, 'client/build', 'index.html');
+        const index = path.join(__dirname, '/client/build', 'index.html');
         res.sendFile(index);
     });
 }
 
 app.use(routes);
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/creelDB";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, function (error) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("mongoose connection successful");
-    }
-});
 
 app.use(passport.initialize());
 
@@ -40,6 +32,10 @@ require("./config/pass")(passport);
 
 app.use("/api/users", users);
 
-app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
+db.sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+        console.log(`🌎 ==> API server now on port ${PORT}!`);
+        seeds.up(db.Creel);
+    });
 });
+
